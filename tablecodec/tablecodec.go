5ec7563f84e39cfc2f7de07dc14d243a7fd57c0d
DreaMer963 <wxd963996380@gmail.com>
2020-09-13 22:33:20 +0800

    finish project1

--- tablecodec/tablecodec.go
+++ tablecodec/tablecodec.go
@@ -39,7 +39,7 @@ var (
 const (
 	idLen     = 8
 	prefixLen = 1 + idLen /*tableID*/ + 2
-	// RecordRowKeyLen is public for calculating avgerage row size.
+	// RecordRowKeyLen is public for calculating average row size.
 	RecordRowKeyLen       = prefixLen + idLen /*handle*/
 	tablePrefixLength     = 1
 	recordPrefixSepLength = 2
@@ -72,7 +72,41 @@ func EncodeRowKeyWithHandle(tableID int64, handle int64) kv.Key {
 // DecodeRecordKey decodes the key and gets the tableID, handle.
 func DecodeRecordKey(key kv.Key) (tableID int64, handle int64, err error) {
 	/* Your code here */
-	return
+	// consider key error
+	if len(key) < tablePrefixLength {
+		return 0, 0, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	if !checkSlicesSame(key[0:tablePrefixLength], tablePrefix) {
+		return 0, 0, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	key = key[tablePrefixLength:]
+	key, tableID, err = codec.DecodeInt(key)
+	if err != nil {
+		return 0, 0, errors.Trace(err)
+	}
+	// consider key error
+	if !checkSlicesSame(key[0:recordPrefixSepLength], recordPrefixSep) {
+		return 0, 0, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	key = key[recordPrefixSepLength:]
+	key, handle, err = codec.DecodeInt(key)
+	if err != nil {
+		return 0, 0, errors.Trace(err)
+	}
+	return tableID, handle, nil
+}
+
+// check equality of two slice
+func checkSlicesSame(slice1 []byte, slice2 []byte) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+	for i, v := range slice1 {
+		if v != slice2[i] {
+			return false
+		}
+	}
+	return true
 }
 
 // appendTableIndexPrefix appends table index prefix  "t[tableID]_i".
@@ -95,6 +129,28 @@ func EncodeIndexSeekKey(tableID int64, idxID int64, encodedValue []byte) kv.Key 
 // DecodeIndexKeyPrefix decodes the key and gets the tableID, indexID, indexValues.
 func DecodeIndexKeyPrefix(key kv.Key) (tableID int64, indexID int64, indexValues []byte, err error) {
 	/* Your code here */
+	// consider key error
+	if len(key) < tablePrefixLength {
+		return 0, 0, nil, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	if !checkSlicesSame(key[0:tablePrefixLength], tablePrefix) {
+		return 0, 0, nil, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	key = key[tablePrefixLength:]
+	key, tableID, err = codec.DecodeInt(key)
+	if err != nil {
+		return 0, 0, nil, errors.Trace(err)
+	}
+	// consider key error
+	if !checkSlicesSame(key[0:len(indexPrefixSep)], indexPrefixSep) {
+		return 0, 0, nil, errInvalidRecordKey.GenWithStack("invalid record key: %q", key)
+	}
+	key = key[len(indexPrefixSep):]
+	key, indexID, err = codec.DecodeInt(key)
+	if err != nil {
+		return 0, 0, nil, errors.Trace(err)
+	}
+	indexValues = key
 	return tableID, indexID, indexValues, nil
 }
 
