4dc8f7c001bf0ca4e8c42999568afadb2608ab16
jp2475 <jp2475@cornell.edu>
2022-02-16 16:43:56 -0500

    lab1

--- tablecodec/tablecodec.go
+++ tablecodec/tablecodec.go
@@ -72,7 +72,21 @@ func EncodeRowKeyWithHandle(tableID int64, handle int64) kv.Key {
 // DecodeRecordKey decodes the key and gets the tableID, handle.
 func DecodeRecordKey(key kv.Key) (tableID int64, handle int64, err error) {
 	/* Your code here */
-	return
+	tableID, _, isRecord, err := DecodeKeyHead(key)
+	if err != nil {
+		return 0, 0, errors.Trace(err)
+	}
+	if !isRecord {
+		err = errInvalidIndexKey.GenWithStack("invalid index key - %q", key)
+		return 0, 0, err
+	}
+	encodedRowId := key[prefixLen:]
+	_, handle, err = codec.DecodeInt(encodedRowId)
+	if err != nil {
+		err = errInvalidIndexKey.GenWithStack("invalid index key - %q %v", key, err)
+		return 0, 0, err
+	}
+	return tableID, handle, nil
 }
 
 // appendTableIndexPrefix appends table index prefix  "t[tableID]_i".
@@ -138,7 +152,6 @@ func DecodeValuesBytesToStrings(b []byte) ([]string, error) {
 	}
 	return datumValues, nil
 }
-
 
 // EncodeRow encode row data and column ids into a slice of byte.
 // Row layout: colID1, value1, colID2, value2, .....
